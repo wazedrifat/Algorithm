358ece0838c80dda4e297333725a96c37de79933
Wazed Rifat <wazedrifat@gmail.com>
2019-02-03 20:07:43 +0600

    up

--- DP/dp.cpp
+++ DP/dp.cpp
@@ -1,3 +1,12 @@
+//	ncr
+for(int i=1 ; i<=n ; i++)
+{
+	dp[i][0]=dp[i][i]=1;
+	for(int j=1 ; j<i ; j++)
+		dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+}
+
+
 //	0/1 Knapsack problem
 if(TotalWeight < weight[i])
 	dp[i][TotalWeight]=dp[i-1][TotalWeight];
--- string processing/KMP algorithm.cpp
+++ string processing/KMP algorithm.cpp
@@ -8,14 +8,11 @@ void lps_array(string pattern)//time : o(n)
 {
 	int i=1, j=0;
 	lps[0]=0;
-	
+
 	while( i<pattern.length() )
 	{
 		if(pattern[j]==pattern[i])
-		{
-			lps[i]=++j;
-			i++;
-		}
+			lps[i++]=++j;
 		else
 			if(j)	j=lps[j-1];
 			else	lps[i++]=0;
@@ -26,7 +23,7 @@ int Search(string text, string pattern)//time : o(n)
 {
 	cnt.clear();
 	lps_array(pattern);
-	
+
 	int i=0, j=0;//i for text & j for pattern
 	while( i<text.length() )
 	{
@@ -34,18 +31,18 @@ int Search(string text, string pattern)//time : o(n)
 		{
 			i++;	j++;
 		}
-		
+
 		if(j==pattern.length())
 		{
 			cnt.push_back(i-j);
 			j=lps[j-1];
 		}
-		
+
 		if(pattern[j]!=text[i])
 			if(j)	j=lps[j-1];
 			else	i++;
 	}
-	
+
 	if(cnt.size())	return cnt[0];
 	else 			return -1;
 }
@@ -54,9 +51,9 @@ int main()
 {
 	string text="ABABDABACDABABCABAB";
     cout<<Search(text, "ABABCABAB")<<endl;
-    
+
     text="bcabdabdababdababd";
-    
+
     cout<<Search(text, "abdababd")<<endl;
     for(int i=0 ; i<cnt.size() ; i++)
     cout<<cnt[i]<<"  ";
